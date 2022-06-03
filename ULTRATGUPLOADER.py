547ebb730eb844cf16dda3c96c2fc3d819450f94
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-06-03 15:36:15 +0530

    Update ULTRATGUPLOADER.py

--- ULTRATGUPLOADER.py
+++ ULTRATGUPLOADER.py
@@ -60,7 +60,7 @@ async def uploadvid(client, message):
       await msg.edit_text(f"https://telegra.ph{tlink[0]}")     
       os.remove(vid_path)   
     except:
-      await msg.edit_text("Please ask @DYNA_OPTIMISE") 
+      await msg.edit_text("Please ask @DYNA_SUPPORT") 
   else:
     await message.reply_text("Size Should Be Less Than **5 mb**")
 
@@ -71,25 +71,26 @@ async def home(client, message):
         InlineKeyboardButton('Close', callback_data='close')
     ],
     [
-        InlineKeyboardButton('Our Channel', url='http://t.me/dyna_NETWORK'),
+        InlineKeyboardButton('Our Channel', url='http://t.me/DYNA_NETWORK'),
         InlineKeyboardButton('Made By', url='https://t.me/DYNA_OPTIMISE')
     ],
     [
-        InlineKeyboardButton("Repository", url='https://github.com/SUKHPAL443/ULTRATGUPLOADER')
+        InlineKeyboardButton("Repository", url='https://github.com')
     ]]
 
   reply_markup = InlineKeyboardMarkup(buttons)
   await Tgraph.send_message(
         chat_id=message.chat.id,
-        text="""<b>Hello, I AM DYNA PIC UPLOADER,
+        text="""<b>Hello, I AM DYNA,
         
 A telegraph Uploader That Can Upload Photo, Video And Gif
 Simply send me photo, video or gif to upload to Telegraph
         
-Made By ~ @DYNA_OPTIMISE</b>""",
+Made By ~ DYNA</b>""",
         reply_markup=reply_markup,
         parse_mode="html",
-        reply_to_message_id=message.message_id)
+        reply_to_message_id=message.message_id
+    )
 
 @Tgraph.on_message(filters.command(["help"]))
 async def help(client, message):
@@ -99,7 +100,7 @@ async def help(client, message):
     ],
     [
         InlineKeyboardButton('Our Channel', url='http://t.me/DYNA_NETWORK'),
-        InlineKeyboardButton('Repository', url='https://github.com/SUKHPAL443/ULTRATGUPLOADER')
+        InlineKeyboardButton('Repository', url="https://github.com")
     ]]
   reply_markup = InlineKeyboardMarkup(buttons)
   await Tgraph.send_message(
@@ -107,7 +108,6 @@ async def help(client, message):
         text=""" 
       
      Please Send me a Pic/Video/Gif which is **Below 5mb**
-
 I'll upload it to telegra.ph and give you the direct link""",
         reply_markup=reply_markup,
         parse_mode="html",
